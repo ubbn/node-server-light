641413c002628c2a0a8df7a00c34f4d315a0bcb3
ubbn <ubbn@live.com>
2017-02-08 16:03:01 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var formidable = require('formidable');
+var util = require('util');
+
+var server = http.createServer(function (request, response) {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (request.method.toLocaleLowerCase() == 'post') {
+    processForm(request, response);
+    return;
+  }
+
+  response.end();
+});
+
+function processForm(request, response) {
+  var form = new formidable.IncomingForm();
+  
+  // parses incoming form
+  form.parse(request, function (error, fields) {
+    // no validation is done, writing successful message
+    response.writeHead(200, {
+      'content-type': 'text/plain'
+    });
+
+    response.end(util.inspect({
+      fields: fields
+    }));
+
+    console.log('posted fields:\n');
+    console.log(util.inspect({
+      fields: fields
+    }));
+  });
+}
+
+var port = 4000;
+server.listen(port);
+console.log("listening on port: " + port);
